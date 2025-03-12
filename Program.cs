b26f59ba8ebeaf46f1741999164ea8076af67a77
yagmurozer <yagmur31323@hotmail.com>
2025-03-13 00:13:50 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("ilk sayıyı girin:");
+double num1= Convert.ToDouble(Console.ReadLine());
+Console.Write("ikinci sayıyı girin:");
+double num2 = Convert.ToDouble(Console.ReadLine());
+double result = num1 + num2;
+Console.WriteLine($"sonuç: {result}");
